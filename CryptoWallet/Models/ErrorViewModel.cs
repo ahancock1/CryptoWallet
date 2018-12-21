3d3c68928a74ac55dacafe4ad539a0840489b752
Adam Hancock <a.hancock@hotmail.co.uk>
2018-12-21 10:44:20 +0000

    Add project files.

---
+++ CryptoWallet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CryptoWallet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
